85ccb11fc613bdddb983242d57161146a4a10bb7
Simon Hirscher <public@simonhirscher.de>
2015-08-03 17:05:08 +0200

    Remove Pandoc extensions from the build script that are enabled by default.

--- make_pdf.sh
+++ make_pdf.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 pandoc whitepaper.md \
--f markdown+footnotes+inline_notes \
+-f markdown \
 -t latex --latex-engine=xelatex \
 \
+# Doesn't work?!
+#--number-sections \
+#--number-offset=0 \
 --filter pandoc-citeproc \
 \
 -s -o whitepaper.pdf

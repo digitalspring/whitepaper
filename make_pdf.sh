da417c20da6d32f90f4c33ed3da14726f701556d
Simon Hirscher <public@simonhirscher.de>
2015-08-04 21:22:43 +0200

    Fix make_pdf.sh

--- make_pdf.sh
+++ make_pdf.sh
@@ -3,10 +3,10 @@ pandoc whitepaper.md \
 -f markdown \
 -t latex --latex-engine=xelatex \
 \
-# Doesn't work?!
-#--number-sections \
-#--number-offset=0 \
 --filter pandoc-citeproc \
 \
 -s -o whitepaper.pdf
 
+# Doesn't work?!
+#--number-sections \
+#--number-offset=0 \

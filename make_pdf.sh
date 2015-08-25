df1ccbedc30165edf75f4fea3250ae47393da402
Simon Hirscher <public@simonhirscher.de>
2015-08-25 12:38:16 -0400

    Provide git commit hash and date as Pandoc variables.
    (They need to be inserted in the custom template later.)

--- make_pdf.sh
+++ make_pdf.sh
@@ -1,10 +1,16 @@
 #!/bin/sh
+gitcommit=$(git rev-parse HEAD)
+gitdate=$(git show -s --format=%ci)
+
 pandoc whitepaper.md \
 -f markdown \
 -t latex --latex-engine=xelatex \
 \
 --filter pandoc-citeproc \
 --chapters \
+\
+--variable gitcommit "$gitcommit"
+--variable gitdate "$gitdate"
 \
 -s -o whitepaper.pdf
 
